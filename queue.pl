3d10ee459e53e6f80e0b94084fbce75d4db996ee
Alan Chen <cmjethz@gmail.com>
2022-11-05 04:22:28 +0800

    Create queue.pl

---
+++ queue.pl
@@ -0,0 +1,26 @@
+/* queue.pl ADT */
+
+empty_queue([]).
+
+enqueue(E,[],[E]).
+enqueue(E,[H|T],[H|TNew]) :-
+	enqueue(E,T,TNew]).
+
+dequeue(E,[E|T],T).
+dequeue(E,[E|T],_).   /* peek */
+
+member_queue(E,Q) :-
+    member(E,Q).
+
+add_list_to_queue(List,Queue,NewQueue) :- 
+	append(Queue,List,NewQueue).
+
+
+
+
+
+
+
+
+
+
