9502b4cb7f59e7ed2b7e107aca2327b372bdb120
Alan Chen <cmjethz@gmail.com>
2022-11-30 00:35:33 +0800

    Create mergeSort.pl

---
+++ mergeSort.pl
@@ -0,0 +1,16 @@
+merge(List, List, []).
+merge(List, [], List).
+merge([MinList1|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
+  MinList1 =< MinList2,
+  merge(RestMerged,RestList1,[MinList2|RestList2]).
+merge([MinList2|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
+  MinList2 =< MinList1,
+  merge(RestMerged,[MinList1|RestList1],RestList2).
+
+
+
+
+
+
+
+
