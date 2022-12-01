a4cd5f752bb340132e0e484f8deff30090253a61
Alan Chen <cmjethz@gmail.com>
2022-12-01 11:39:54 +0800

    Update mergeSort.pl

--- mergeSort.pl
+++ mergeSort.pl
@@ -2,10 +2,26 @@ merge(List, List, []).
 merge(List, [], List).
 merge([MinList1|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
   MinList1 =< MinList2,
-  merge(RestMerged,RestList1,[MinList2|RestList2]).
+
+merge(RestMerged,RestList1,[MinList2|RestList2]).
 merge([MinList2|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
   MinList2 =< MinList1,
   merge(RestMerged,[MinList1|RestList1],RestList2).
+
+
+mergeSort([], []).
+mergeSort([A], [A|[]]).
+
+mergeSort(Sorted, List) :-
+  length(List, N),
+  FirstLength is //(N, 2),
+  SecondLength is N - FirstLength,
+  length(FirstUnsorted, FirstLength),
+  length(SecondUnsorted, SecondLength),
+  append(FirstUnsorted, SecondUnsorted, List),
+  mergeSort(FirstSorted, FirstUnsorted),
+  mergeSort(SecondSorted, SecondUnsorted),
+  merge(Sorted, FirstSorted, SecondSorted).
 
 
 
