f9f941993f24edb8c59009c21a6d162e948fd410
Alan Chen <cmjethz@gmail.com>
2023-04-03 22:57:54 +0800

    Update stack.pl

--- stack.pl
+++ stack.pl
@@ -1,26 +1,9 @@
 /* stack.pl ADT */
 
-empty_stack([]).
+empty([]).
+push(Top, Stack, [Top|Stack]).
+pop(Top, [Top|Stack],Stack).
 
-stack(Top, Stack, [Top|Stack]).   /* used for push, pop and peek */
-
-member_stack(Element, Stack) :-  member(Element,Stack).
-
-
-add_list_to_stack(List,Stack,Result) :- 
-	append(List,Stack,Result).
-
-
-reverse_print_stack(S) :- empty_stack(S).
-reverse_print_stack(S) :- 
-	stack(E,Rest,S),
-	reverse_print_stack(Rest),
-	write(E),nl.
-
-
-member(H,[H|T]).
-member(X,[_|T]) :- member(X,T).
-  
   
   
   
